2ebd0e31ee3561383f06e4567cfb2887b82d0a84
sadiqss <sadiq.sau48@gmail.com>
2022-04-27 01:50:51 +0100

    finished

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
             res.send(result);
         })
 
+        //DELETE
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
